97821bc43b75c77075c1699bf12b3125e53ccba2
unknown <josemferrando@hotmail.com>
2020-10-21 09:36:10 -0300

    Modificacion nombre de variables

--- OperacionesMatematicas_Resultado.php
+++ OperacionesMatematicas_Resultado.php
@@ -6,19 +6,19 @@
 
   <?php
   switch($_REQUEST['radio1']){
-    case "suma":
+    case "Suma":
       $suma = $_REQUEST['valor1'] + $_REQUEST['valor2'];
       echo "La suma es:" . $suma;
     break;
-    case "resta":
+    case "Resta":
       $resta = $_REQUEST['valor1'] - $_REQUEST['valor2'];
       echo "La resta es:" . $resta;
     break;
-    case "multiplicar":
+    case "Multiplicar":
       $multiplicacion = $_REQUEST['valor1'] * $_REQUEST['valor2'];
       echo "La multiplicacion es:" . $multiplicacion;
     break;
-    case "division":
+    case "Division":
       $division = $_REQUEST['valor1'] / $_REQUEST['valor2'];
       echo "La division es:" . $division;
     break;
